50b82ce3c2379a8624428e94c513117bf6611057
kapil2545 <kapil@webanix.in>
2024-08-18 10:38:08 +0530

    build issue fix

--- src/components/ExploreCourseWorkSection.tsx
+++ src/components/ExploreCourseWorkSection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import CourseWorkCard from "@/components/CourseWorkCard.tsx";
+import CourseWorkCard from "@/components/CourseWorkCard";
 
 function ExploreCourseWorkSection() {
   interface CourseWorkData {
